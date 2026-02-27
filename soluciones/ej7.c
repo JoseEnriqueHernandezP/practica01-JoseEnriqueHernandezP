1708b26352b0f310ff51ae9d2b7cd93683e25d4c
Enrique Hernandez <piedraquique@gmail.com>
2026-02-26 22:58:32 -0600

    Ejercicio 7 Completado!!
    Ejercicio 7 Completado!!

--- soluciones/ej7.c
+++ soluciones/ej7.c
@@ -1,6 +1,18 @@
-
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 7
+    int base =0;
+    int columnas = 4;
+    int size = 4;  
+    int dir_m03 =base +((0*columnas) +3) * size;
+    int dir_m10 =base +((1*columnas) +0) * size ;
+
+    printf("Formula:\n");
+    printf("Dir(m[i][j])=Base+((i * columnas) + j) * sizeof(tipo)\n\n");
+    printf("m[0][3]= %d\n", dir_m03) ;
+    printf("m[1][0]= %d\n\n", dir_m10);
+    if(dir_m10 > dir_m03)
+        printf("&m[1][0] es mayor.\n") ;
+    else
+        printf("&m[0][3] es mayor.\n") ; 
     return 0;
 }
