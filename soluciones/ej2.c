a35bce1725a492efaff8a46cbbee356e10b83be7
Enrique Hernandez <piedraquique@gmail.com>
2026-02-26 21:46:47 -0600

    Solucion Ejercicio 2
    Completè el ejercicio 2.

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -1,6 +1,9 @@
-
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 2
+    int a [ 6 ] ;
+ 
+    for ( int i=4; i <6; i++){
+        printf ( "La direccion de a[%i] es:%p\n",i, &a[i]) ;
+    }
     return 0;
 }
