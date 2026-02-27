b63006d28f4ed257595363dc0aa6a3ebba11e61a
Enrique Hernandez <piedraquique@gmail.com>
2026-02-26 22:04:39 -0600

    Ejercicio 4 Completado!!
    Ejercicio 4 Completado!!

--- soluciones/ej4.c
+++ soluciones/ej4.c
@@ -1,6 +1,22 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 4
-    return 0;
+
+int main() {
+
+    int dir_a0= 5000;
+    int dir_a3= 5012;
+
+    int tamaño= (dir_a3-dir_a0)/3;
+    int dir_a5= dir_a0+5*tamaño;
+
+    printf("Salida:\n");
+    printf("&a[0]= %d\n",dir_a0 ) ;
+    printf("&a[3]= %d\n\n",dir_a3 );
+    printf("Calcula:\n");
+    printf("Tamaño del tipo: %d BYTES\n", tamaño );
+    printf("Direccion de a[5]: %d\n", dir_a5 );
+
+    printf( "\nFormula usada:\n" );
+    printf( "Direccion a[i]=Direccion a[0] + i * sizeof(tipo)\n" );
+    printf( "Aplicando: 5000+5*4=5020\n" );
+    return 0 ;
 }
