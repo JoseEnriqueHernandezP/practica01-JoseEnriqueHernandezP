4833e1cf896d5ba8d7b2c40706114d935226f920
Enrique Hernandez <piedraquique@gmail.com>
2026-02-26 21:31:08 -0600

    Ejercicio 1 completado
    He escrito las soluciones del código 1

--- soluciones/ej1.c
+++ soluciones/ej1.c
@@ -1,6 +1,10 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 1
+    int a[6];
+    for(int i=0; i<6; i++){
+        printf("La direccion del a[% i] es: %p\n ", i ,&a [i] );
+    }
     return 0;
+    
 }
