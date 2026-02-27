ad1838108da683f4aca7b147b67552c770b1c374
Enrique Hernandez <piedraquique@gmail.com>
2026-02-26 23:49:33 -0600

    Ejercicio 12 Completado!!
    Ejercicio 12 Completado!!

--- soluciones/ej12.c
+++ soluciones/ej12.c
@@ -1,6 +1,16 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 12
+int main() {
+    int base = 4000 ;
+    int columnas = 5;
+    int size = 4; 
+
+    int dir_m24 =base +((2 * columnas +4)* size);
+    int dir_m13 =base +((1 * columnas +3)* size) ;
+    int diferencia= dir_m24-dir_m13;
+    printf("Base = %d\n\n", base)  ;
+
+    printf ("Direccion de la m[2][4] = %d\n", dir_m24);
+    printf ("Direccion de la m[1][3] = %d\n", dir_m13)  ;
+    printf ("Diferencia = %d bytes\n", diferencia);
     return 0;
 }
