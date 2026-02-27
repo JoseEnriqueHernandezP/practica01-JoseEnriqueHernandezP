f1dd0412de4d11398e89849ceac5b81d24011861
Enrique Hernandez <piedraquique@gmail.com>
2026-02-26 22:21:58 -0600

    Ejercicio 6 Completado!!
    Ejercicio 6 Completado!!

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,11 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 6
-    return 0;
+    int m [2] [ 4];
+    for(int i=0; i<2;i++){
+        for( int j=0; j<4; j++){
+            printf("\nLa Direccion de m[%i][%i] : %p\n", i,j, &m[i][j]);
+        }
+    }
+ return 0;
 }
