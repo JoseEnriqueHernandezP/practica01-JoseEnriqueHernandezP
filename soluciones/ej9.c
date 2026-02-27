1f6b9d37a80293181615e169abda8a4d5e180dd0
Enrique Hernandez <piedraquique@gmail.com>
2026-02-26 23:17:11 -0600

    Ejercicio 9 Completado!!
    Ejercicio 9 Completado!!

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,10 @@
-
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 9
+    int a[5];
+    printf("Direccion de memoria en: %p",&a [-1]) ;
+    printf ("\n") ;
+    printf ("Direccion de memoria enn: %p",&a [5]) ;
     return 0;
+
+    
 }
