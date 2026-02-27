a2f684d733c874b5f39bff53d97d5d9caf06a208
Enrique Hernandez <piedraquique@gmail.com>
2026-02-26 23:35:22 -0600

    Ejercicio 11 Completado!!
    Ejercicio 11 Completado!!

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -1,6 +1,11 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 11
+int main() {
+    int base = 1000 ;
+    int tamaño = 8 ;   
+    printf(" Salida:\n") ;
+    for(int i = 0; i < 3; i++) {
+        int direccion = base + (i * tamaño);
+        printf("&a[%d] = %d\n", i, direccion);
+    }
     return 0;
 }
